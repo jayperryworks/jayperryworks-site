336bd0e3f6cd8500dba6c97f528dfcef688b54b6
Jay Perry <j@jayperryworks.com>
2019-03-26 13:33:56 -0400

    [#41] first web component rendering!

---
+++ source/components/print-edition.js
@@ -0,0 +1,40 @@
+const template = document.createElement('template')
+
+template.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      color: white;
+      background-color: red;
+    }
+  </style>
+  <div class="overflow-hidden">
+    <figure class="gutter-wrapper gutter-wide">
+      <figcaption class="gutter">
+        <slot name="image">Add an image</slot>
+        <slot name="title">Add a title</slot>
+        <dl class="stats">
+          <div class="stats-group">
+            <dt class="stats-title">
+              <slot name="stat-label">Add a stat label here</slot>
+            </dt>
+            <dd class="stat">
+              <slot name="stat">Add a stat here</slot>
+            </dd>
+          </div>
+        </dl>
+      </figcaption>
+    </figure>
+  </div>
+`
+
+class PrintEdition extends HTMLElement {
+  constructor() {
+    super()
+
+    this.attachShadow({mode: 'open'})
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+}
+
+customElements.define('print-edition', PrintEdition)
