adfbfbf471b4bb587bcb080fe4696c42a1a3be4b
Jay Perry <j@jayperryworks.com>
2019-10-12 00:02:50 -0400

    [#84] stub out picture post page with real data!

--- src/routes/pictures/[year]/[slug].svelte
+++ src/routes/pictures/[year]/[slug].svelte
@@ -71,15 +71,41 @@
 
 <article>
   <header>
+    <img src="{post.cover[0].path}" alt="{post.title}">
     <h1>{post.title}</h1>
     <time datetime='{format(new Date(date.year), 'yyyy')}'>
       {format(new Date(date.year), 'yyyy')}
     </time>
+    <p>{post.format}{#if post.width && post.height}&nbsp;&bull; {post.width}" x {post.height}"{/if}</p>
   </header>
 
   {#if post.intro}
     <div class='content'>
       {@html post.intro}
     </div>
+  {/if}
+
+  {#if post.editions}
+    <section>
+      <h2>Available editions</h2>
+      {#each post.editions as edition}
+        <ul>
+          <li>
+            <img src="{edition.photo[0].path}" alt="{edition.name}">
+            <aside>
+              <h3>{edition.name}</h3>
+              <dl>
+                <dt>Type</dt>
+                <dd>{edition.type}</dd>
+                <dt>Size</dt>
+                <dd>{edition.width}" x {edition.height}"</dd>
+                <dt>Paper size</dt>
+                <dd>{edition.width + edition.border}" x {edition.height + edition.border}"</dd>
+              </dl>
+            </aside>
+          </li>
+        </ul>
+      {/each}
+    </section>
   {/if}
 </article>
