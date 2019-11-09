a972a4dd2431a6b660b92d0f556d5d611fbf7e87
Jay Perry <j@jayperryworks.com>
2019-11-09 12:11:21 -0500

    [#84] set up Gallery component and prints page layout

--- src/routes/pictures/index.svelte
+++ src/routes/pictures/index.svelte
@@ -11,12 +11,18 @@
 
 <script>
   import { format } from 'date-fns'
-  import PageTitle from '../../components/PageTitle.svelte'
+  import PageTitle from '@/components/PageTitle.svelte'
+  import OutdentedBlurb from '@/components/OutdentedBlurb.svelte'
+  import Gallery from '@/components/Gallery.svelte'
 
   export let pictures
 </script>
 
 <style>
+  h1 {
+    margin-top: -0.38em;
+  }
+
   ul {
     margin: 0 0 1em 0;
     line-height: 1.5;
@@ -25,18 +31,43 @@
 
 <PageTitle title="Pictures" />
 
-<h1>Recent work</h1>
+<OutdentedBlurb class="padding-x-outside padding-y-xwide">
+  <div slot="blurb" class="padding-bottom-wide">
+    <h1 class="padding-bottom-narrow">Recent work</h1>
+    <a href="https://jayperry.etsy.com">More prints avaialble at <strong>Etsy</strong> &rsaquo;</a>
+  </div>
 
-<ul>
-  {#each pictures as picture}
-    <li>
-      <a
-        rel='prefetch'
-        href='{`pictures/${picture.date.year}/${picture.slug}`}'
-      >
-        <img src="{picture.thumbnail[0].path}" alt="{picture.thumbnail[0].alt}">
-        <h2>{picture.title}</h2>
-      </a>
-    </li>
-  {/each}
-</ul>
+  <div slot="body" class="overflow-hidden">
+    <Gallery>
+      {#each pictures as picture}
+        <li>
+          <a
+            rel='prefetch'
+            href='{`pictures/${picture.date.year}/${picture.slug}`}'
+          >
+            <img src="{picture.thumbnail[0].path}" alt="{picture.thumbnail[0].alt}">
+            <h2>{picture.title}</h2>
+          </a>
+        </li>
+        <li>
+          <a
+            rel='prefetch'
+            href='{`pictures/${picture.date.year}/${picture.slug}`}'
+          >
+            <img src="{picture.thumbnail[0].path}" alt="{picture.thumbnail[0].alt}">
+            <h2>{picture.title}</h2>
+          </a>
+        </li>
+        <li>
+          <a
+            rel='prefetch'
+            href='{`pictures/${picture.date.year}/${picture.slug}`}'
+          >
+            <img src="{picture.thumbnail[0].path}" alt="{picture.thumbnail[0].alt}">
+            <h2>{picture.title}</h2>
+          </a>
+        </li>
+      {/each}
+    </Gallery>
+  </div>
+</OutdentedBlurb>
