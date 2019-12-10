d8536669857b10614234672a154802015e0d3102
Jay Perry <j@jayperryworks.com>
2019-12-09 21:59:17 -0500

    [#84] tweak blog listings to include read more btn

--- src/routes/writing/index.svelte
+++ src/routes/writing/index.svelte
@@ -11,6 +11,8 @@
 
 <script>
   import { format } from 'date-fns'
+  import arrow from 'icons/arrow-right.svg'
+  import Icon from '@/components/Icon.svelte'
   import OutdentedBlurb from '@/components/OutdentedBlurb.svelte'
   import PageTitle from '@/components/PageTitle.svelte'
   import PostBody from '@/components/PostBody.svelte'
@@ -18,6 +20,10 @@
   import Wrapper from '@/components/Wrapper.svelte'
 
   export let posts
+
+  function date(date, template = 'MMMM d, yyyy') {
+    return format(new Date(date.year, date.month, date.day), template)
+  }
 </script>
 
 <style type="text/scss">
@@ -48,14 +54,15 @@
     <Wrapper centered={false}>
       <ul class="
         list-undecorated
-        padding-y-between-wide
         margin-y-between-wide
+        padding-y-between-wide
       ">
         {#each posts as post}
           <li>
             <a
               rel="prefetch"
               href={post.path}
+              class="t-link-undecorated"
             >
               {#if post.cover}
                 <div class="padding-bottom">
@@ -68,25 +75,42 @@
               <h2>{post.title}</h2>
               {#if post.subtitle}
                 <p class="
+                  c-fg-tertiary
                   t-font-accent
                   t-scale-gamma
-                  c-fg-tertiary
                 ">{post.subtitle}</p>
               {/if}
               <time
                 class="
+                  c-fg-tertiary
                   display-block
                   padding-y-xnarrow
                   t-font-accent
-                  t-weight-bold
                   t-scale-zeta
-                  c-fg-tertiary
+                  t-weight-bold
                 "
-                datetime={format(new Date(post.date.year, post.date.month, post.date.day), 'yyyy-M-dd')}
+                datetime={date(post.date, 'yyyy-M-dd')}
               >
-                {format(new Date(post.date.year, post.date.month, post.date.day), 'MMMM d, yyyy')}
+                {date(post.date)}
               </time>
               <PostBody sections={post.excerpt} dropCap={false} />
+              {#if post.readMore}
+                <span class="
+                  display-inline-block
+                  padding-top
+                  t-case-upper
+                  t-font-accent
+                  t-scale-zeta
+                  t-weight-bold
+                ">
+                  Read more
+                  <Icon
+                    svg={arrow}
+                    margin="left"
+                    size="small"
+                  />
+                </span>
+              {/if}
             </a>
           </li>
         {/each}
