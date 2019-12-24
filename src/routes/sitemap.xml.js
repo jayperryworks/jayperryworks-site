d1d745a73ff9c94766b9a39952a379bd8be85b17
Jay Perry <j@jayperryworks.com>
2019-12-24 00:59:08 -0500

    [#99] set up sitemap server route working when i point a browser at it, but not generating on export

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,74 +1,60 @@
+// https://github.com/sveltejs/v2.svelte.dev/blob/8aad909631fd94e203f14ac3cf168c3c97c0ecd5/src/routes/blog/rss.xml.js
+// https://github.com/sveltejs/hn.svelte.technology/blob/master/src/routes/%5Blist%5D/rss.js
+// https://github.com/sveltejs/sapper/issues/461
+import fs from 'fs'
+import yaml from 'js-yaml'
 import generatePictureList from '@/utils/generatePictureList.js'
 import generateWritingList from '@/utils/generateWritingList.js'
-import { format } from 'date-fns'
+// import { formatISO } from 'date-fns'
 
-const siteurl = 'https://jayperryworks.com'
+const siteData = yaml.safeLoad(
+  fs.readFileSync('content/site.yml', 'utf-8')
+)
 
-const pages = [
-  {
-    title: 'Home',
-    path: '',
-    date: {
-      year: 2019,
-      month: 12,
-      day: 16
-    },
-    priority: 1,
-  },
-  {
-    title: 'About',
-    path: 'about',
-    date: {
-      year: 2019,
-      month: 12,
-      day: 16
-    },
-    priority: 0.5,
-  },
-  {
-    title: 'Prints & paintings',
-    path: 'pictures',
-    date: {
-      year: format(new Date(), 'YYYY'),
-      month: format(new Date(), 'M'),
-      day: format(new Date(), 'd')
-    },
-    priority: 0.8,
-    change: 'monthly'
-  },
-  {
-    title: 'Writing',
-    path: 'writing',
-    date: {
-      year: format(new Date(), 'YYYY'),
-      month: format(new Date(), 'M'),
-      day: format(new Date(), 'd')
-    },
-    priority: 0.8,
-    change: 'monthly'
-  }
-]
+function generatePagesList() {
+  return siteData.pages.map(page => {
+    if (page.data) {
+      const pageData = yaml.safeLoad(
+        fs.readFileSync(page.data, 'utf-8')
+      )
 
-function render(pages, siteurl) {
+      return {
+        date: pageData.date,
+        ...page
+      }
+    }
+
+    return page
+  })
+}
+
+// ${page.date && `<lastmod>${formatISO(new Date(page.date))}</lastmod>`}
+function render(pages) {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${
-        pages.map((page) => {
-          `
+        pages.map(page => `
             <url>
-              <loc>${siteurl}/${page.path}</loc>
-              <lastmod></lastmod>
-              <changefreq>${page.date.year || '2020'}-${page.date.month || '01'}-${page.date.day || '01'}</changefreq>
-              <priority>${page.priority || 0.5}</priority>
+              <loc>${siteData.url}/${page.path}</loc>
+              <changefreq>${page.change || 'yearly'}</changefreq>
+              <priority>${page.priority || 0.6}</priority>
             </url>
           `
-        })
+        )
       }
     </urlset>
-  `
+  `.trim()
 }
 
 export function get(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/xml'
+  })
 
+  res.end(render([
+    ...generatePagesList(),
+    ...generatePictureList('content/pictures'),
+    ...generateWritingList('content/writing')
+  ]))
 }
