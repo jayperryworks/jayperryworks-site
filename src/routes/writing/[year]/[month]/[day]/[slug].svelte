4c4d52033afac1cd258e423b8f65e58e72efb2db
Jay Perry <j@jayperryworks.com>
2020-03-01 11:43:16 -0500

    convert spaces to tabs on a couple files

--- src/routes/writing/[year]/[month]/[day]/[slug].svelte
+++ src/routes/writing/[year]/[month]/[day]/[slug].svelte
@@ -1,80 +1,80 @@
 <script context="module">
-  export async function preload({ params, query }) {
-    const { year, month, day, slug } = params
-    // the `slug` parameter is available because
-    // this file is called [slug].svelte
-    const response = await this.fetch(`writing/${year}/${month}/${day}/${slug}.json`);
-    const data = await response.json()
+	export async function preload({ params, query }) {
+		const { year, month, day, slug } = params
+		// the `slug` parameter is available because
+		// this file is called [slug].svelte
+		const response = await this.fetch(`writing/${year}/${month}/${day}/${slug}.json`);
+		const data = await response.json()
 
-    if (response.status !== 200) {
-      this.error(response.status, data.message)
-      return
-    }
+		if (response.status !== 200) {
+			this.error(response.status, data.message)
+			return
+		}
 
-    return {
-      post: data,
-      date: {
-        year: Number(year),
-        month: (Number(month) - 1),
-        day: Number(day)
-      }
-    }
-  }
+		return {
+			post: data,
+			date: {
+				year: Number(year),
+				month: (Number(month) - 1),
+				day: Number(day)
+			}
+		}
+	}
 </script>
 
 <script>
-  import { format } from 'date-fns'
-  import Card from '@/components/Card.svelte'
-  import Cover from '@/components/Cover.svelte'
-  import Figure from '@/components/Figure.svelte'
-  import PageTitle from '@/components/PageTitle.svelte'
-  import PostBody from '@/components/PostBody.svelte'
-  import ResponsiveImage from '@/components/ResponsiveImage.svelte'
-  import Wrapper from '@/components/Wrapper.svelte'
+	import { format } from 'date-fns'
+	import Card from '@/components/Card.svelte'
+	import Cover from '@/components/Cover.svelte'
+	import Figure from '@/components/Figure.svelte'
+	import PageTitle from '@/components/PageTitle.svelte'
+	import PostBody from '@/components/PostBody.svelte'
+	import ResponsiveImage from '@/components/ResponsiveImage.svelte'
+	import Wrapper from '@/components/Wrapper.svelte'
 
-  export let post;
-  export let date;
+	export let post;
+	export let date;
 
-  function formatDate(formatString) {
-    return format(new Date(date.year, date.month, date.day), formatString)
-  }
+	function formatDate(formatString) {
+		return format(new Date(date.year, date.month, date.day), formatString)
+	}
 </script>
 
 <PageTitle title="{post.title}" />
 
 <article class="padding-x-outside padding-y-xwide">
-  <header>
-    <Wrapper
-      width="wide"
-      class="t-align-center@small"
-    >
-      <h1>{post.title}</h1>
-      {#if post.subtitle}
-        <p class="c-fg-tertiary padding-top-xxnarrow t-font-accent t-heading t-scale-beta">
-          {post.subtitle}
-        </p>
-      {/if}
+	<header>
+		<Wrapper
+			width="wide"
+			class="t-align-center@small"
+		>
+			<h1>{post.title}</h1>
+			{#if post.subtitle}
+				<p class="c-fg-tertiary padding-top-xxnarrow t-font-accent t-heading t-scale-beta">
+					{post.subtitle}
+				</p>
+			{/if}
 
-      {#if post.cover}
-        <Cover
-          class="padding-top-wide"
-          sources={post.cover.sources}
-          alt={post.cover.alt}
-          caption={post.cover.caption}
-          credit={post.cover.credit}
-          border={post.cover.border}
-        />
-      {/if}
-    </Wrapper>
-    <Wrapper class="border-bottom margin-bottom padding-bottom-xnarrow padding-top-xwide">
-      <time
-        class="c-fg-tertiary t-font-accent t-scale-zeta t-weight-bold"
-        datetime={formatDate('yyyy-M-dd')}
-      >
-        {formatDate('MMMM d, yyyy')}
-      </time>
-    </Wrapper>
-  </header>
+			{#if post.cover}
+				<Cover
+					class="padding-top-wide"
+					sources={post.cover.sources}
+					alt={post.cover.alt}
+					caption={post.cover.caption}
+					credit={post.cover.credit}
+					border={post.cover.border}
+				/>
+			{/if}
+		</Wrapper>
+		<Wrapper class="border-bottom margin-bottom padding-bottom-xnarrow padding-top-xwide">
+			<time
+				class="c-fg-tertiary t-font-accent t-scale-zeta t-weight-bold"
+				datetime={formatDate('yyyy-M-dd')}
+			>
+				{formatDate('MMMM d, yyyy')}
+			</time>
+		</Wrapper>
+	</header>
 
-  <PostBody sections={post.body} />
+	<PostBody sections={post.body} />
 </article>
