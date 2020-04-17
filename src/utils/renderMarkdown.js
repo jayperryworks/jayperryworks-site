9108af9ab0a59ecb8d9b30d357100fe11b97ceaa
Jay Perry <j@jayperryworks.com>
2020-04-17 00:24:58 -0400

    clean up markdown util found a better way on another project

--- src/utils/renderMarkdown.js
+++ src/utils/renderMarkdown.js
@@ -3,26 +3,21 @@ const markdown = require('markdown-it')
 const footnotes = require('markdown-it-footnote')
 
 module.exports = (
-  content,
-  {
-    inline = false,
-    html = false
-  } = {}
+	content,
+	{
+		inline = false,
+		html = false
+	} = {}
 ) => {
-  // markdown-it options
-  const options = { typographer: true, html: html }
+	// markdown-it options
+	const options = {
+		typographer: true,
+		html
+	}
 
-  // if the 'inline' option is true, strip out p tags
-  // and leave out block-level plugins (e.g. footnotes)
-  // -> renderer surrounds all text with p tags by default
-  if (inline) {
-    return markdown(options)
-      .render(content)
-      .replace(/<\/?p[^>]*>/g, '')
-  }
-
-  // otherwise assume it's body text
-  return markdown(options)
-    .use(footnotes)
-    .render(content)
+	// if the 'inline' option is true, render without surrounding p tag
+	// and leave out block-level plugins (e.g. footnotes)
+	return inline
+		? markdown(options).renderInline(content)
+		: markdown(options).use(footnotes).render(content)
 }
