fc664757f7d1ab15dde793468e058883362e7639
Jay Perry <j@jayperryworks.com>
2020-04-29 00:58:23 -0400

    [#87] resize images on about page

--- src/routes/about.json.js
+++ src/routes/about.json.js
@@ -1,13 +1,34 @@
 import fs from 'fs'
 import yaml from 'js-yaml'
-import render from '@/utils/renderPostBody.js'
+import renderPostBody from '@/utils/renderPostBody.js'
+import resizeImage from '@/utils/resizeImage.js'
 
-export function get(req, res) {
+export async function get(req, res) {
   let data = yaml.safeLoad(
     fs.readFileSync('content/about.yml', 'utf-8')
   )
 
-  renderPostBody(data.body)
+  // resize the cover image
+  if (data.cover && data.cover.image) {
+  	data.cover.image = await resizeImage(data.cover.image)
+  }
+
+  data.body = renderPostBody(data.body)
+
+  // resize body images
+  await Promise.all(data.body.map(async (block) => {
+  	// if it has an 'image' field (e.g. figure), resize it
+  	if (block.image) {
+  		block.image = await resizeImage(block.image)
+  	}
+
+  	if (block.images) {
+  		// if it has an 'images' field (e.g. gallery), resize each
+  		block.images.map(async (item) => {
+				item.image = await resizeImage(item.image)
+  		})
+  	}
+  }))
 
   res.writeHead(200, {
     'Content-Type': 'application/json'
