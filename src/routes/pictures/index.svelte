dd4a821388e6dc532f7a15843ea5b51f6dee942a
Jay Perry <j@jayperryworks.com>
2020-07-28 00:02:33 -0400

    Style the sorting and refine code #150

--- src/routes/pictures/index.svelte
+++ src/routes/pictures/index.svelte
@@ -32,16 +32,7 @@
   // get unique series values from pictures array
   // and remove empty/undefined values
   // -> https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates#14438954
-  $: series = [...new Set(pictures.map(picture => picture.series))].filter(p => p)
-  $: unsortedPictures = pictures.filter(picture => !picture.series)
-  // $: picturesBySeries = {
-  // 	...series.reduce((result, name) => {
-  // 			result[name] = pictures.filter(picture => picture.series === name)
-  // 			return result
-  // 	}, {}),
-  // 	unsorted: pictures.filter(picture => !picture.series)
-  // }
-
+  $: series = content.series
   $: picturesBySeries = [
   	...series.map((name) => {
   		return {
@@ -49,15 +40,12 @@
   			pictures: pictures.filter(picture => picture.series === name)
   		}
   	}),
+  	// unsorted pictures (no series)
   	{
   		name: false,
   		pictures: pictures.filter(picture => !picture.series)
   	}
   ]
-
-  function getPicturesBySeries (name) {
-  	return pictures.filter(picture => picture.series === name)
-  }
 </script>
 
 <style>
@@ -76,7 +64,7 @@
     bodyWidth="wide"
   >
     <div slot="blurb" class="padding-bottom-wide">
-      <h1 class="padding-bottom-narrow">{content.title}</h1>
+      <h1 class="padding-bottom">{content.title}</h1>
       {#if content.intro}
       <Wrapper
         centered={false}
@@ -87,37 +75,39 @@
         </div>
       </Wrapper>
       {/if}
-      <Button
-        href={content.shop}
-        target="_blank"
-        iconRight={arrow}
-      >
-        Find more prints at <strong>Etsy</strong>
-      </Button>
     </div>
 
-    <div slot="body" class="overflow-hidden">
-    	{#each picturesBySeries as series}
-    		{#if series.name}
-	    		<h2 class="">{titleize(series.name)} series</h2>
-    		{/if}
-	      <Gallery>
-	        {#each series.pictures as picture}
-	          <li>
-	            <a
-	              class="t-link-undecorated"
-	              rel="prefetch"
-	              href="{picture.path}"
-	            >
-	              <ResponsivePicture
-	                sources={picture.thumbnail.versions}
-	                alt={picture.title}
-	                border
-	              />
-	            </a>
-	          </li>
-	        {/each}
-	      </Gallery>
+    <div
+    	slot="body"
+    	class="margin-y-between-wide padding-y-between-wide overflow-hidden"
+  	>
+    	{#each picturesBySeries as series, index}
+    		<section class:border-top="{index > 0}">
+    			{#if series.name}
+		    		<h2 class="t-scale-delta t-font-accent c-fg-tertiary padding-bottom-narrow"><strong>{titleize(series.name)}</strong> series</h2>
+    			{/if}
+		      <Gallery>
+		        {#each series.pictures as picture}
+		          <li>
+		            <a
+		              class="t-link-undecorated"
+		              rel="prefetch"
+		              href="{picture.path}"
+		            >
+		              <ResponsivePicture
+		                sources={picture.thumbnail.versions}
+		                alt={picture.title}
+		                border
+		              />
+		            </a>
+		          </li>
+		        {/each}
+		        {#if series.pictures.length < 4}
+		          <li><!-- filler item for scaling --></li>
+		          <li><!-- filler item for scaling --></li>
+	          {/if}
+		      </Gallery>
+    		</section>
     	{/each}
     </div>
   </OutdentedBlurb>
