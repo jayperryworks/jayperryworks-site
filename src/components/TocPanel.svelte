36a713098f65a57d7ad5fc3cb89f5a09725dcd83
Jay Perry <j@jayperryworks.com>
2020-11-18 10:42:32 -0500

    set up toc panel content #166

---
+++ src/components/TocPanel.svelte
@@ -0,0 +1,48 @@
+<script>
+	import arrowDotted from 'icons/arrow-dotted-right.svg'
+	import Icon from '@/components/Icon.svelte'
+	import Wrapper from '@/components/Wrapper.svelte'
+
+	export let link, // url the panel should point to
+		heading, // the title, e.g. "Blog"
+		number // sequence number, e.g. 01
+
+	// append additional classes as needed
+	let classes = ''
+	export { classes as class }
+</script>
+
+<style type="text/scss">
+	article {
+		min-height: 70vh;
+	}
+</style>
+
+<article class="border-seam-top | padding-y-xwide padding-x-outside | {classes}">
+	<Wrapper width="xwide">
+    <header class="padding-bottom-xwide">
+    	<p class="t-scale-gamma t-font-accent | c-fg-tertiary | padding-bottom-narrow">
+    		{number}
+    	</p>
+    	<h2 class="padding-bottom t-scale-alpha">
+        <a href={link}>{heading}</a>
+      </h2>
+      <a
+      	class="t-link-undecorated margin-x padding-top-narrow position-relative"
+      	href={link}
+    	>
+        <span class="c-fg-tertiary display-inline-block t-case-upper t-font-accent t-scale-eta t-weight-bold">
+          See more
+        </span>
+        <span class="card-icon">
+          <Icon
+            svg={arrowDotted}
+            size="xlarge"
+            class="no-margin-top"
+          />
+        </span>
+      </a>
+    </header>
+    <slot></slot>
+  </Wrapper>
+</article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,83 +1,75 @@
 <script context="module">
-  export async function preload() {
-    const response = await this.fetch('index.json')
-    const data = await response.json()
+	export async function preload() {
+		const response = await this.fetch('index.json')
+		const data = await response.json()
 
-    if (response.status !== 200) {
-      this.error(response.status, data.message)
-      return
-    }
+		if (response.status !== 200) {
+			this.error(response.status, data.message)
+			return
+		}
 
-    return { content: data }
-  }
+		return { content: data }
+	}
 </script>
 
 <script>
-  import { onDestroy } from 'svelte'
-  import arrow from 'icons/arrow-right.svg'
-  import arrowDotted from 'icons/arrow-dotted-right.svg'
-  import Button from '@/components/Button.svelte'
-  import Card from '@/components/HomeTOCCard.svelte'
-  import Gallery from '@/components/Gallery.svelte'
-  import Icon from '@/components/Icon.svelte'
-  import MainNav from '@/components/MainNav.svelte'
-  import PageTitle from '@/components/PageTitle.svelte'
-  import ResponsivePicture from '@/components/ResponsivePicture.svelte'
-  import Wrapper from '@/components/Wrapper.svelte'
+	import { onDestroy } from 'svelte'
+	import { format } from 'date-fns'
+	import arrow from 'icons/arrow-right.svg'
+	import Button from '@/components/Button.svelte'
+	import Card from '@/components/HomeTOCCard.svelte'
+	import Gallery from '@/components/Gallery.svelte'
+	import Icon from '@/components/Icon.svelte'
+	import MainNav from '@/components/MainNav.svelte'
+	import PageTitle from '@/components/PageTitle.svelte'
+	import ResponsivePicture from '@/components/ResponsivePicture.svelte'
+	import TocPanel from '@/components/TocPanel.svelte'
+	import Wrapper from '@/components/Wrapper.svelte'
 
-  export let content
+	export let content
 
-  // --- "Favorite things" (I like ... ) rotating subheading ---
-  let favoriteThings = []
+	// --- "Favorite things" (I like ... ) rotating subheading ---
+	let favoriteThings = []
 
-  function randomFavorites () {
-    favoriteThings = Object.keys(content.favoriteThings).map((type) => {
-      const list = content.favoriteThings[type]
-      return list[Math.floor(Math.random() * list.length)]
-    })
-  }
+	function randomFavorites () {
+		favoriteThings = Object.keys(content.favoriteThings).map((type) => {
+			const list = content.favoriteThings[type]
+			return list[Math.floor(Math.random() * list.length)]
+		})
+	}
 
-  $: favoriteThingsSubhead = `I like ${
-    favoriteThings.map((thing, index) => {
-      if (index == (favoriteThings.length - 1)) {
-        return `and ${thing}`
-      }
-      return thing
-    }).join(', ')
-  }.`
+	$: favoriteThingsSubhead = `I like ${
+		favoriteThings.map((thing, index) => {
+			if (index == (favoriteThings.length - 1)) {
+				return `and ${thing}`
+			}
+			return thing
+		}).join(', ')
+	}.`
 
-  randomFavorites()
-  const cycleFavorites = setInterval(randomFavorites, 5000)
-  onDestroy(() => clearInterval(cycleFavorites))
+	randomFavorites()
+	const cycleFavorites = setInterval(randomFavorites, 5000)
+	onDestroy(() => clearInterval(cycleFavorites))
+
+	function date(date, template = 'MM/dd') {
+		return format(new Date(date.year, date.month, date.day), template)
+	}
 </script>
 
 <style type="text/scss">
-  .home {
-    display: block;
+	.home {
+		display: block;
 
-    @supports (display: flex) {
-      display: flex;
-      flex-direction: column;
-      flex: 1;
-    }
-  }
+		@supports (display: flex) {
+			display: flex;
+			flex-direction: column;
+			flex: 1;
+		}
+	}
 
-  .intro {
-  	min-height: 70vh;
-  }
-
-  .toc-item {
-  	min-height: 100vh;
-  }
-
-  .card-icon {
-    display: inline-block;
-    position: absolute;
-    right: 0;
-    top: 50%;
-    transform: translateY(-20%);
-    vertical-align: middle;
-  }
+	.intro {
+		min-height: 70vh;
+	}
 </style>
 
 <PageTitle />
@@ -85,60 +77,65 @@
 <MainNav overlay />
 
 <div class="home">
-  <section class="intro | display-flex display-flex-column display-flex-justify-center | padding-top-xwide">
+	<section class="intro | display-flex display-flex-column display-flex-justify-center | padding-top-xwide">
 
-    <!-- intro -->
-    <div class="padding-x-outside">
-    	<Wrapper width="xwide">
-        <Wrapper centered={false}>
-          <h1 class="t-scale-beta">{content.intro}</h1>
-          {#if favoriteThings.length > 0}
-            <p class="c-fg-tertiary | margin-top-xnarrow | t-font-accent t-heading t-scale-gamma">
-              {favoriteThingsSubhead}
-            </p>
-          {/if}
-          <Button
-            prefetch={true}
-            href="about"
-            iconRight={arrow}
-            class="margin-top"
-          >
-            A bit more about me
-          </Button>
-        </Wrapper>
-      </Wrapper>
-    </div>
-  </section>
+		<!-- intro -->
+		<div class="padding-x-outside">
+			<Wrapper width="xwide">
+				<Wrapper centered={false}>
+					<h1 class="t-scale-beta">{content.intro}</h1>
+					{#if favoriteThings.length > 0}
+						<p class="c-fg-tertiary | margin-top-xnarrow | t-font-accent t-heading t-scale-gamma">
+							{favoriteThingsSubhead}
+						</p>
+					{/if}
+					<Button
+						prefetch={true}
+						href="about"
+						iconRight={arrow}
+						class="margin-top"
+					>
+						A bit more about me
+					</Button>
+				</Wrapper>
+			</Wrapper>
+		</div>
+	</section>
 
-  <!-- TOC -->
-  <section>
-    <header class="padding-bottom padding-x-outside">
-    	<Wrapper width="xwide">
-        <h2 class="t-case-upper t-font-accent t-scale-eta t-weight-bold">
-          Table of contents
-        </h2>
-      </Wrapper>
-    </header>
-    {#each content.tableOfContents as item}
-      <article class="toc-item | border-seam-top | padding-y-xwide padding-x-outside">
-      	<Wrapper width="xwide">
-          <h2 class="padding-bottom t-scale-alpha">
-            <a href={item.link}>{item.heading}</a>
-          </h2>
-          <div class="margin-x padding-top-narrow position-relative">
-            <span class="c-fg-tertiary display-inline-block t-case-upper t-font-accent t-scale-eta t-weight-bold">
-              Read more
-            </span>
-            <span class="card-icon">
-              <Icon
-                svg={arrowDotted}
-                size="xlarge"
-                class="no-margin-top"
-              />
-            </span>
-          </div>
-		    </Wrapper>
-      </article>
-    {/each}
-  </section>
+	<!-- TOC -->
+	<section>
+		<header class="padding-bottom padding-x-outside">
+			<Wrapper width="xwide">
+				<h2 class="t-case-upper t-font-accent t-scale-eta t-weight-bold">
+					Table of contents
+				</h2>
+			</Wrapper>
+		</header>
+		{#each content.tableOfContents as item, i}
+			<TocPanel number="0{i + 1}" link={item.link} heading={item.heading}>
+				{#if item.content.type == 'image'}
+					{#each item.content.images as image}
+						{image}
+					{/each}
+				{/if}
+				{#if item.content.type == 'list'}
+					<Gallery gutter="wide">
+						{#each item.content.posts as post}
+							<li>
+								<time
+									class="c-fg-tertiary | display-block | padding-bottom-xnarrow | t-font-accent t-scale-eta"
+									datetime={date(post.date, 'yyyy-M-dd')}
+								>
+									{date(post.date)}
+								</time>
+								<h4 class="t-scale-gamma">
+									<a href="{post.path}">{post.title}</a>
+								</h4>
+							</li>
+						{/each}
+					</Gallery>
+				{/if}
+			</TocPanel>
+		{/each}
+	</section>
 </div>
