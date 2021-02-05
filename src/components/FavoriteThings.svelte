bacdb250d4d57261d410084528a6b30d16a57e65
Jay Perry <j@jayperryworks.com>
2021-02-05 17:09:36 -0500

    getting closer

--- src/components/FavoriteThings.svelte
+++ src/components/FavoriteThings.svelte
@@ -5,6 +5,9 @@
 	export let speed = 2000
 
 	let currentListIndex = 0
+	$ : previousListIndex = currentListIndex > 0 
+		? currentListIndex - 1 
+		: list.length - 1
 
 	function updateFavoriteThings() {
 		if (currentListIndex === list.length - 1) {
@@ -19,6 +22,10 @@
 </script>
 
 <style>
+	dl {
+		font-size: 0;
+	}
+
 	dt,
 	dd {
 		margin-left: 0;
@@ -28,6 +35,11 @@
 	dt + dd,
 	dd + dd {
 		margin-left: 0.2em;
+	}
+
+	dd {
+		position: relative;
+		opacity: 0;
 	}
 
 	dd::after {
@@ -43,21 +55,53 @@
 	}
 
 	.set {
-		display: none;
-		transition: all 0.25s ease-in-out;
+		--delay: 0.25s;
+		--duration: 0.25s;
+		transition: visibility 0 ease-in-out 2s;
+		position: relative;
+		overflow: hidden;
+		visibility: hidden;
+	}
+
+	.set > dd {
+		transition: 
+			opacity var(--duration) ease-in-out var(--delay, 0), 
+			top var(--duration) ease-in-out var(--delay, 0);
+		top: 2em;
+		opacity: 0;
 	}
 
 	.current {
-		display: block;
+		visibility: visible;
+	}
+
+	.current > dd {
+		top: 0;
+		opacity: 1;
+	}
+
+	.previous > dd {
+		top: -2em;
 	}
 </style>
 
-<dl class="c-fg-tertiary | margin-top-xnarrow | t-font-accent t-heading t-scale-gamma | list-undecorated">
+<dl class="margin-top-xnarrow | list-undecorated">
 	{#each list as set, index}
-		<div class="set" class:current={index === currentListIndex}>
-			<dt>I like</dt>
-			{#each set as word}
-				<dd>{word}</dd>
+		<div 
+			class="set" 
+			class:current={index === currentListIndex}
+			class:previous={index === previousListIndex}
+		>
+			<dt class="t-font-accent t-heading t-scale-gamma c-fg-tertiary">
+				I like
+			</dt>
+			{#each set as word, index}
+				<dd 
+					class="t-font-accent t-heading t-scale-gamma c-fg-tertiary"
+					style="--delay: {index * 0.1}s;"
+				>
+					{word}
+				</dd>
 			{/each}
 		</div>
 	{/each}
