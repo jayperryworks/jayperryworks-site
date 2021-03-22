e6daa6f866c8901955c2448ed6555c30a9eea0bd
Jay Perry <j@jayperryworks.com>
2021-03-22 11:46:07 -0400

    update node version and init design tokens data

---
+++ content/design-tokens.config.js
@@ -0,0 +1,25 @@
+const palette = {
+  black: '#444',
+  gray: '#b1b1b1',
+  lavendar: '#7999ec'
+}
+
+const type = {
+  fonts: {
+    display: '',
+    body: ''
+  },
+  scale: {
+
+  }
+}
+
+const spacing = {
+  xxxnarrow: scale.get(-12),
+  xxnarrow: scale.get(-8),
+  xnarrow: scale.get(-4),
+  narrow: scale.get(-2),
+  medium: scale.get(1),
+  wide: scale.get(3),
+  xwide: scale.get(6)
+}
