0fda1fbc03c166dbd7fbada3fd65bf18cab79259
Jay Perry <j@jayperryworks.com>
2021-06-25 12:42:21 -0400

    begin cleaning up button styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -19,10 +19,10 @@
   {href}
   {rel}
   {target}
-  class="button type-font-accent {role} {size} {classes}"
-  class:has-icon-left={iconLeft}
-  class:has-icon-right={iconRight}
-  class:type-scale-zeta={size !== 'large'}
+  class="button {role} {size} {classes}"
+  class:has-icon="{iconLeft || iconRight}"
+  class:left="{iconLeft}"
+  class:right="{iconRight}"
 >
   {#if iconLeft}
     <Icon
@@ -45,21 +45,25 @@
 
 <style>
   .button {
-    --padding-x: 1.2em;
-    --padding-y: 0.5em;
-    --icon-offset: 0.3em;
     --color: var(--color-primary);
+    --font-size: var(--type-scale-epsilon);
+    --icon-offset: 0.3em;
+    --padding-x: 1.2em;
+    --padding-y: 0.57em;
 
+    background-color: transparent;
+    border-radius: 0.2em;
     border: 1px solid currentColor;
     border: 1px solid var(--color);
     color: currentColor;
     color: var(--color);
-    transition: all 0.25s ease;
-    background-color: transparent;
-    border-radius: 0.2em;
     display: inline-block;
-    padding: var(--padding-x) var(--padding-y);
+    font-family: var(--type-font-accent);
+    font-size: 1rem; /* fallback reset */
+    font-size: var(--font-size);
+    padding: var(--padding-y) var(--padding-x);
     position: relative;
+    transition: all 0.25s ease;
   }
 
   .button:hover,
@@ -78,29 +82,30 @@
 
   /* --- sizes --- */
   .xsmall {
+    --font-size: var(--type-scale-zeta);
     --icon-offset: 0;
     --padding-x: 0.6em;
     --padding-y: 0.2em;
   }
 
   .small {
+    --font-size: var(--type-scale-zeta);
     --icon-offset: 0.13em;
     --padding-x: 0.8em;
     --padding-y: 0.3em;
   }
 
-  .large {
-    --padding-x: 1.4em;
-    --padding-y: 0.6em;
-  }
-
   /* --- icons --- */
-  .button.has-icon-left {
-    padding-left: calc(var(--padding-x) + 1em);
+  .button.has-icon {
+    --icon-padding: 1.6em;
+  }
+  
+  .button.has-icon.left {
+    padding-left: calc(var(--padding-x) + var(--icon-padding));
   }
 
-  .button.has-icon-right {
-    padding-right: calc(var(--padding-y) + 1em);
+  .button.has-icon.right {
+    padding-right: calc(var(--padding-y) + var(--icon-padding));
   }
 
   .button :global(.icon) {
