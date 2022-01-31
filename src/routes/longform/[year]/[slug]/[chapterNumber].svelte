70fe04b435f9806ed64bdd172e5c293ea876c5ec
Jay Perry <j@jayperryworks.com>
2022-01-31 11:57:17 -0500

    set up pagination and cover markup

--- src/routes/longform/[year]/[slug]/[chapterNumber].svelte
+++ src/routes/longform/[year]/[slug]/[chapterNumber].svelte
@@ -11,11 +11,14 @@
 		const chapter = await chapterResponse.json();
 
 		return {
+			isCoverPage: (chapterNumber == 1),
 			date: { year },
 			project,
 			chapter,
 			pagination: [
+				// previous chapter
 				project.chapters[chapterNumber- 2],
+				// next chapter
 				project.chapters[chapterNumber]
 			],
 			slug
@@ -30,22 +33,32 @@
 
 	import { format } from 'date-fns';
 
-	export let project, chapter, date, pagination, slug;
+	export let isCoverPage, project, chapter, date, pagination;
+	console.log(isCoverPage)
 
 	$:formattedDate = format(new Date(date.year, 0), 'yyyy');
 </script>
 
 <MainNav segment="writing" />
 <main>
-	<header>
-		<time datetime="{formattedDate}">{formattedDate}</time>
-		{#if chapter.title}
-			<h1>{chapter.title}</h1>
-		{/if}
-		{#if chapter.subtitle}
-			<p>{chapter.subtitle}</p>
-		{/if}
-	</header>
+	{#if isCoverPage}
+		<header>
+			<h1>{project.title}</h1>
+			{#if project.subtitle}
+			<p>{project.subtitle}</p>
+			{/if}
+			<time datetime="{formattedDate}">{formattedDate}</time>
+		</header>
+	{:else}
+		<header>
+			{#if chapter.title}
+				<h1 class="type-scale-beta">{chapter.title}</h1>
+			{/if}
+			{#if chapter.subtitle}
+				<p>{chapter.subtitle}</p>
+			{/if}
+		</header>
+	{/if}
 	<PostBody blocks={chapter.body} />
 	<PaginationNav items="{pagination}" />
 </main>
