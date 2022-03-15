645d22a501fdd91b5a8036a7f3b24ce1c33248ad
Jay Perry <j@jayperryworks.com>
2022-03-15 13:05:12 -0400

    hotfix: commas again - fix typo/refactor

--- src/utils/stringHelpers.js
+++ src/utils/stringHelpers.js
@@ -1,28 +1,38 @@
 // cheers to https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/
-module.exports = {
-	titleize: (string) => {
-	  return string.replace(/^\w/, c => c.toUpperCase());
-	},
-	sentenceCase: (string) => {
-		return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
-	},
-	// convert an array to a sentence
-	// -> adds an oxford comma
-	// -> ['first', 'Second'] => First and second
-	// -> ['first', 'Second', 'THIRD'] => First, second, and third
-	arrayToSentence: (array, { period = true }) => {
-		let string;
+function titleize (string) {
+	return string.replace(/^\w/, c => c.toUpperCase());
+}
 
-		if (array.length <= 2) {
-			string = array.map(s => s.toLowerCase()).join(' and ');
-		}
+function sentenceCase (string) {
+	return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
 
-		string = array.map((item, index) => {
-			const lowercase = item.toLowerCase();
-			if (index < array.length - 1) return lowercase;
-			return `and ${lowercase}`;
-		}).join(', ');
+// convert an array to a sentence
+// -> adds an oxford comma
+// -> ['first', 'Second'] => First and second
+// -> ['first', 'Second', 'THIRD'] => First, second, and third
+function arrayToSentence (array, { period = true } = {}) {
+	const end = period ? '.' : '';
 
-		return period ? `${string}.` : string;
+	// Adjust the case for each string
+	// and prepend 'and' to the last item if there's more than 1 item
+	array = array.map((string, index) => {
+		if (index === 0) return sentenceCase(string);
+		if (index === array.length - 1) return `and ${string.toLowerCase()}`;
+		return string.toLowerCase();
+	});
+
+	// if there's only 2 items, don't add commas
+	if (array.length <= 2) {
+		return array.join(' ') + end;
 	}
+
+	// add commas otherwise, including an oxford comma
+	return array.join(', ') + end;
+}
+
+module.exports = {
+	titleize,
+	sentenceCase,
+	arrayToSentence
 }
