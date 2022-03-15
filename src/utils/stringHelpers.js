aa0266cf57d4c202c19489064da112167f9a8eb8
Jay Perry <j@jayperryworks.com>
2022-03-15 11:36:45 -0400

    hotfix: commas not showing up in pictures media

--- src/utils/stringHelpers.js
+++ src/utils/stringHelpers.js
@@ -2,5 +2,27 @@
 module.exports = {
 	titleize: (string) => {
 	  return string.replace(/^\w/, c => c.toUpperCase());
+	},
+	sentenceCase: (string) => {
+		return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+	},
+	// convert an array to a sentence
+	// -> adds an oxford comma
+	// -> ['first', 'Second'] => First and second
+	// -> ['first', 'Second', 'THIRD'] => First, second, and third
+	arrayToSentence: (array, { period = true }) => {
+		let string;
+
+		if (array.length <= 2) {
+			string = array.map(s => s.toLowerCase()).join(' and ');
+		}
+
+		string = array.map((item, index) => {
+			const lowercase = item.toLowerCase();
+			if (index < array.length - 1) return lowercase;
+			return `and ${lowercase}`;
+		}).join(', ');
+
+		return period ? `${string}.` : string;
 	}
 }
