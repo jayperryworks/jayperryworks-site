a4567aa9ca0211c109938a48be9ae9a8dce6eeac
Jay Perry <j@jayperryworks.com>
2022-04-08 10:52:06 -0400

    clear git cache

--- _old/src/components/Note.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let html
-
-  let classes = ''
-  export { classes as class }
-</script>
-
-<aside class="t-style-italic {classes}">{@html html}</aside>
--- _old/src/routes/blog/_layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import MainNav from '@/components/MainNav.svelte'
-</script>
-
-<MainNav segment="blog" />
-<main>
-  <slot></slot>
-</main>
