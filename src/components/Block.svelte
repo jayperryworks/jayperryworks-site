a842371a23333eb6cace55ade8cd304ee1b31edd
Jay Perry <j@jayperryworks.com>
2022-04-27 21:09:54 -0400

    move markdown rendering back to server

--- src/components/Block.svelte
+++ src/components/Block.svelte
@@ -1,36 +1,54 @@
 <script lang="ts">
-	// block components
-	import Passage from '@blocks/Passage.svelte';
-	import Figure from '@blocks/Figure.svelte';
+// block components
+import Passage from '@blocks/Passage.svelte';
+import Figure from '@blocks/Figure.svelte';
 
-	// other components
-	import Markdown from '@components/Markdown.svelte';
-	import ContentWrapper from '@components/ContentWrapper.svelte';
+// other components
+import ContentWrapper from '@components/ContentWrapper.svelte';
 
-	// typescript
-	import { SvelteComponent } from 'svelte';
+// typescript
+import { SvelteComponent } from 'svelte';
 
-	export let block: SvelteComponent;
+export let block: SvelteComponent;
 
-	let className = '';
-	export { className as class };
+let className = '';
+export { className as class };
+
+function getWidth(prominence) {
+	const widths = {
+		small: 'narrow',
+		medium: 'default',
+		large: 'wide',
+	};
+	const index = Object.keys(widths).find((item) => item == prominence);
+	return widths[index] || 'default';
+}
+
+function getBlockClass(type) {
+	// a quote block behaves the same as a passage,
+	// so assign it the same class
+	if (type === 'quote') {
+		type = 'passage';
+	}
+
+	const classes = [
+		'passage',
+		'heading',
+	];
+
+	if (classes.includes(type)) {
+		return `block-${type}`;
+	}
+}
 </script>
 
 <ContentWrapper
-	width={block.prominence || 'default'}
-	class={className}
+	width={getWidth(block.prominence)}
+	class="{getBlockClass()} {className}"
 >
 	<!-- passage -->
 	{#if block.type === 'passage'}
-		<Passage {...block}>
-			{#if block.html}
-				{block.html}
-			{/if}
-
-			{#if block.markdown}
-				<Markdown source={block.markdown} />
-			{/if}
-		</Passage>
+		<Passage {...block} />
 	{/if}
 
 	<!-- figure -->
@@ -38,3 +56,14 @@
 		<Figure {...block} />
 	{/if}
 </ContentWrapper>
+
+<style>
+	.blocks :global(.block-heading + .block-passage) {
+    padding-top: var(--space-narrow);
+  }
+
+  /* when two sections of type follow one another, add "invisible" spacing between so they feel like one continuous flow of text */
+  .blocks :global(.block-passage + .block-passage) {
+    padding-top: var(--space-medium);
+  }
+</style>
--- src/components/blocks/Figure.svelte
+++ src/components/blocks/Figure.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
-	import Image from '../Image.svelte';
+import Image from '@components/Image.svelte';
+import Caption from '@components/Caption.svelte';
 
-	export let sources: Array<{
-		path: string;
-		width: number;
-		height: number;
-	}>;
-	export let alt: string;
+export let sources: Array<{
+	path: string;
+	width: number;
+	height: number;
+}>;
+
+export let alt: string;
+export let caption: string;
+export let attribution: string;
+export let border = false;
+export let cover = false;
 </script>
 
 <figure class="type-align-center">
-  <Image {sources} {alt}/>
+	<slot>
+		<Image {sources} {border} {cover} {alt}/>
+	</slot>
+
+	{#if caption || attribution}
+		<Caption
+			class="margin-x-auto padding-top-xnarrow | content-width"
+			{attribution}
+		>
+			{@html caption}
+		</Caption>
+	{/if}
 </figure>
+
+<style>
+	figure {
+		margin: 0;
+		text-align: center;
+	}
+</style>
