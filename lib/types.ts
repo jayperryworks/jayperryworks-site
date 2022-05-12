e3af01b45fc57885d9d2dba9a2ef02dfff5b137b
Jay Perry <j@jayperryworks.com>
2022-05-12 10:29:23 -0400

    set up ImageGallery and Gallery components

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,9 @@
 import { RichTextField } from '@prismicio/types';
 
 export type TypeScale = 'alpha' | 'beta' | 'gamma' | 'delta' | 'epsilon' | 'zeta';
+export type SpaceScale = 'xxnarrow' | 'xnarrow' | 'narrow' | 'medium' | 'wide' | 'xwide';
 export type Prominence = 'Small' | 'Medium' | 'Large';
+export type GallerySize = 'small' | 'medium' | 'large';
 export type ImageFit = 'default' | 'contain' | 'cover';
 
 export interface Block {
