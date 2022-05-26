1f57159596bc909ef7d5505166f62c9424cc78be
Jay Perry <j@jayperryworks.com>
2022-05-26 11:37:38 -0400

    fix typescript syntax in component props

--- lib/types.ts
+++ lib/types.ts
@@ -8,15 +8,15 @@ export type GallerySize = 'small' | 'medium' | 'large';
 export type ImageFit = 'default' | 'contain' | 'cover';
 
 export interface Block {
-	type: string,
-	includeInExcerpt?: boolean,
-	prominence?: Prominence,
+	type: string;
+	includeInExcerpt?: boolean;
+	prominence?: Prominence;
 	// the arbitrary props for any given Block component, defined on each component
 	// e.g. a Passage with a prismicText prop
-	[key: string]: unknown,
+	[key: string]: unknown;
 }
 
 export interface FormattedText {
-	prismicText?: RichTextField,
-	markdown?: string,
+	prismicText?: RichTextField;
+	markdown?: string;
 }
