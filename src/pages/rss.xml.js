cb60aec46e6f69ad84a1ef16d34272f302947bc6
Jay Perry <j@jayperryworks.com>
2022-09-15 15:54:32 -0400

    set up blog items and start on pictures

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -49,18 +49,34 @@ const pictures = await prismic.getAllByType('picture', {
 	`,
 });
 
+function pictureDescription
+
+const pictureItems = pictures.map(({ uid, data, tags }) => ({
+	link: `${import.meta.env.SITE}${picture({ data, uid })}`,
+	title: prismicHelpers.asText(data.title),
+	pubDate: prismicHelpers.asDate(data.date_completed),
+	description: `A new ${tags.include('painting') ? 'painting' : 'drawing'}${data.series?.data?.title ? `Part of the ${data.series.data.title} series.`: ''}. ${data.editions ? 'Print editions available.' : ''}`,
+}));
+
+console.log(pictureItems);
+
 export const get = () => rss({
 	// `<title>` field in output xml
 	title: 'Jay Perry',
 	// `<description>` field in output xml
-	description: "Jay's recent writing, drawings, and random thoughts",
+	description: "Jay's recent writing and drawings",
 	// base URL for RSS <item> links
 	// SITE will use "site" from your project's astro.config.
 	site: import.meta.env.SITE,
 	// list of `<item>`s in output xml
 	// simple example: generate items for every md file in /src/pages
 	// see "Generating items" section for required frontmatter and advanced use cases
-	items: import.meta.glob('./**/*.md'),
+	items: blogPosts.map(({ uid, data }) => ({
+		link: `${import.meta.env.SITE}${blogPost({ data, uid })}`,
+		title: prismicHelpers.asText(data.title),
+		pubDate: prismicHelpers.asDate(data.date),
+		description: data.description ? prismicHelpers.asText(data.description) : undefined,
+	})),
 	// (optional) inject custom xml
 	customData: '<language>en-us</language>',
 	stylesheet: '/rss/styles.xsl',
