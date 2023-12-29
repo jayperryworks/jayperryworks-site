4c69134256f6aa3d9ea2d0eca7b893f53d996fb5
Jay Perry <jayperryworks@users.noreply.github.com>
2023-12-29 21:09:31 +0000

    set up matte and background colors from CMS data

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
+// import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
 import svgLoader from 'vite-svg-loader';
 
@@ -16,13 +16,13 @@ export default defineConfig({
 		]
 	},
 	integrations: [
-		compress({
-			svg: false,
-			html: {
-				removeComments: true,
-				removeEmptyAttributes: true,
-			},
-		}),
+		// compress({
+		// 	svg: false,
+		// 	html: {
+		// 		removeComments: true,
+		// 		removeEmptyAttributes: true,
+		// 	},
+		// }),
 		sitemap(),
 	]
 });
