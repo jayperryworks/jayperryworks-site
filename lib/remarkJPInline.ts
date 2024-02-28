210055abe4aa591dae60e7f8e551d385e734f0ea
Jay Perry <j@jayperryworks.com>
2024-02-28 10:52:46 -0500

    add JSDoc comment to inline plugin

--- lib/remarkJPInline.ts
+++ lib/remarkJPInline.ts
@@ -1,5 +1,15 @@
 import { visit, SKIP } from 'unist-util-visit';
 
+/**
+ * Remark plugin: render markdown as 'inline', without surrounding p tags
+ * @date 2/28/2024 - 10:49:50 AM
+ *
+ * @export
+ * @param {Object} options
+ * @param {boolean} [options.renderInline] pass 'true' to activate this plugin
+ * @returns {(tree: any) => void}
+ */
+
 export default function remarkJPInline(options) {
 	const { renderInline } = options;
 
