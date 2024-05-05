12a33f4ec524d64761708c25e08f92a4916c3e36
Jay Perry <jayperryworks@users.noreply.github.com>
2024-05-05 01:45:02 +0000

    one more step in the pseudo code

--- netlify/functions/books.mjs
+++ netlify/functions/books.mjs
@@ -5,3 +5,4 @@
 // 4. If not, connect the OpenLibrary API and query for the book's ISBN
 // 5. Add the book's info to the blob
 // 6. Download its cover image, make versions, and save to the blob (or maybe do this on Astro?)
+// 7. Send the books data as JSON as response
