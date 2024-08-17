35be56879112de886fdfa8d22649435c97fc02b3
Jay Perry <jayperryworks@users.noreply.github.com>
2024-08-17 21:12:48 +0000

    trying to merge all the data but it's not working - need to just handle OL data

--- netlify/functions/books.mjs
+++ netlify/functions/books.mjs
@@ -1,13 +1,39 @@
 // run `netlify dev` to test locally
 
 // On request:
-// 1. Pull books records from Prismic/CMS
-// 2. Loop through them and check to see if each has metadata in the blob
-// 3. If so, continue
-// 4. If not, connect the OpenLibrary API and query for the book's ISBN
-// 5. Add the book's info to the blob
-// 6. Download its cover image, make versions, and save to the blob (or maybe do this on Astro?)
+// 1. Check to see if a blob exists for the book UID
+// 3. If so, return it as JSON
+// 4. If not, connect the OpenLibrary API and query for the book's ISBN/OLID
+// 5. Add the book's info to the blob under the given UID
+// 6. Download its cover image, make versions, and save to the blob
 // 7. Send the books data as JSON as response
+
+/**
+ * @typedef {Object} OpenLibraryData
+ * @property {String} url - OpenLibrary URL
+ * @property {String} publishDate - publish date for this edition
+ * @property {String} cover - url of the cover image
+ * @property {String} publisher - name of the publisher
+ * @property {Boolean} hasOpenLibraryData - flag: have we successfully loaded OL data?
+ */
+
+/**
+ * @typedef {Object} Book
+ * @property {String} uid - unique identifier for the book
+ * @property {Object} uid.actions - links to buy/view/borrow the book
+ * @property {String} uid.actions.purchase - url to buy the book, e.g. Bookshop.org
+ * @property {String} uid.actions.openLibrary - url to find the book on OpenLibrary
+ * @property {Boolean} uid.hasOpenLibraryData - flag: have we successfully loaded OL data?
+ * @property {Number} uid.isbn - ISBN number
+ * @property {Object} uid.relatedContent - related content from Prismic
+ * @property {String[]} uid.tags - list of tags for the book
+ * @property {String} uid.author - author's name
+ * @property {String} uid.cover - url of the cover image
+ * @property {String} uid.olid - OpenLibrary ID
+ * @property {String} uid.publishDate - publish date for this edition
+ * @property {String} uid.publisher - name of the publisher
+ * @property {String} uid.title - title of the book
+ */
 
 import * as prismicHelpers from '@prismicio/helpers';
 import * as prismic from '@prismicio/client';
@@ -18,13 +44,23 @@ const prismicEndpoint = process.env.PRISMIC_REPOSITORY;
 const prismicToken = process.env.PRISMIC_TOKEN;
 const contactEmail = process.env.CONTACT_EMAIL;
 
-async function queryOpenLibraryData(data = {}) {
+/**
+ * Try to fetch book data from OpenLibrary
+ *
+ * @async
+ * @param {Object} data - book data needed to reach OL endpoints
+ * @param {Number} [data.isbn] - the book's ISBN number
+ * @param {String} [data.olid] - the book's Open Library ID
+ * @returns {OpenLibraryData}
+ */
+async function queryOpenLibraryData(data) {
 	const {
 		isbn,
 		olid,
 	} = data;
 
-	// try to fetch additional data from OpenLibrary
+	// -> if there's an isbn number, use OL's ISBN api endpoint
+	// -> if not, use OL's ID number in their regular book search api
 	const openLibraryEndpoint = isbn
 		? `https://openlibrary.org/isbn/${isbn}.json`
 		: `https://openlibrary.org/books/${olid}.json`;
@@ -61,7 +97,14 @@ async function queryOpenLibraryData(data = {}) {
 	return { hasOpenLibraryData: false };
 }
 
-export default async function () {
+/**
+ * Return JSON data from the 'books' blob
+ *
+ * @export
+ * @async
+ * @returns {Bookstore}
+ */
+export default async function() {
 	// store of cached book data
 	const bookStore = getStore('books');
 
@@ -77,29 +120,37 @@ export default async function () {
 		},
 	});
 
-	const books = await Promise.all(booksResponse.map(async ({
+	await Promise.all(booksResponse.map(async ({
 		uid,
 		tags,
 		data,
 	}) => {
-		const cachedBook = await bookStore.getWithMetadata(uid);
+		const cachedBook = await bookStore.get(uid);
 
+		// if the book already exists in the cache (blob)...
 		if (cachedBook) {
-
+			// if the book is cached but does not have OL data...
 			if (!cachedBook.hasOpenLibraryData) {
 				// query openlibrary for data and add it
 				const openLibraryData = await queryOpenLibraryData(data);
+				console.log('no OL data', cachedBook.data);
 
-				// bookData.actions.openlibrary = openLibraryData.url;
-				// delete openLibraryData.url;
+				const actions = {
+					...cachedBook.actions,
+					openLibrary: openLibraryData.url,
+				};
 
-				// bookData = {
-				// 	...bookData,
-				// 	...openLibraryData,
-				// };
+				delete openLibraryData.url;
+
+				await bookStore.setJSON(uid, {
+					...cachedBook,
+					...openLibraryData,
+					actions,
+				});
 			}
 
-			return cachedBook;
+			// stop and use the cached data
+			return;
 		}
 
 		const {
@@ -111,7 +162,7 @@ export default async function () {
 			title,
 		} = data;
 
-		// query the Open Library API with the book's ISBN number
+		// format the prismic data if there's an ISBN or OLID available
 		if (isbn || olid) {
 			let bookData = {
 				isbn,
@@ -126,34 +177,34 @@ export default async function () {
 				uid,
 			};
 
+			// query the Open Library API
 			const openLibraryData = await queryOpenLibraryData(bookData);
 
 			if (openLibraryData) {
 				bookData.actions.openlibrary = openLibraryData.url;
 				delete openLibraryData.url;
 
-				bookData = {
+				await bookStore.setJSON(uid, {
 					...bookData,
 					...openLibraryData,
-				};
+				});
 			}
 
-			console.log(bookData);
-			// if the request to OL fails, just use the data we have from the CMS
-			return bookData;
+			// if the request to OpenLibrary fails, just use the data we have
+			await bookStore.setJSON(uid, bookData);
 		}
 	}));
 
 
-	const firstBook = books[0];
-	const firstStoreItem = await bookStore.get(firstBook.uid);
+	// const firstBook = books[0];
+	// const firstStoreItem = await bookStore.get(firstBook.uid);
 
-	if (!firstStoreItem) {
-		await bookStore.setJSON(firstBook.uid, { ...firstBook });
-	}
+	// if (!firstStoreItem) {
+	// 	await bookStore.setJSON(firstBook.uid, { ...firstBook });
+	// }
 
 	return new Response(
-		JSON.stringify(await bookStore.get(firstBook.uid)),
+		JSON.Stringify(await bookStore.list()),
 		{
 			status: 200,
 			headers: {
