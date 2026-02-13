455cea18da3a3be64b7b549b50a1dbc504a20de5
Jay Perry <j@jayperryworks.com>
2026-02-13 17:24:22 -0500

    astro init for design site

---
+++ sites/design/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import sharedConfig from "../../shared/shared.astro.config";
+import svgLoader from "vite-svg-loader";
+
+// https://astro.build/config
+export default defineConfig({
+	...sharedConfig,
+
+	integrations: [sitemap()],
+	site: "https://design.jayperry.works",
+	vite: {
+		plugins: [svgLoader()],
+		envDir: "../../shared",
+	},
+});
