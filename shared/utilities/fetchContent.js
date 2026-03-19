c8aa9dd57ac11dc0d6af06bf9f591a0f8b84ce0e
Jay Perry <j@jayperryworks.com>
2026-03-19 12:36:53 -0400

    got book data & covers rendering

--- shared/utilities/fetchContent.js
+++ shared/utilities/fetchContent.js
@@ -1,10 +1,14 @@
 /**
  *
  * @param {string} endpoint - path to the api endpoint, e.g. 'books'
- * @returns {JSON | Error}
+ * @param {'prod'|'dev'} env - which server env to fetch from - defaults to local for dev, live server for prod
+ * @returns {object | Error}
  */
-export default async function (endpoint) {
-	const apiURL = import.meta.env.CONTENT_API_URL;
+export default async function (endpoint, options = {}) {
+	const { env = 'prod' } = options;
+
+	const apiURL =
+		env === 'prod' ? import.meta.env.CONTENT_API_URL : 'http://jpw-api.test';
 	const apiToken = import.meta.env.CONTENT_API_TOKEN;
 
 	try {
