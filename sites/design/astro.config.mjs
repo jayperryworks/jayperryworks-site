1904c5dbe0ac9c89d599d26da10f8de1e12b5f9a
Jay Perry <j@jayperryworks.com>
2026-03-23 16:29:37 -0400

    update config for design and personal sites

--- sites/design/astro.config.mjs
+++ sites/design/astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 import sharedConfig from '../../shared/shared.astro.config';
+import sitemap from '@astrojs/sitemap';
 import svgLoader from 'vite-svg-loader';
 
 // https://astro.build/config
 export default defineConfig({
 	...sharedConfig,
 
+	adapter: netlify(),
 	integrations: [sitemap()],
 	site: 'https://design.jayperry.works',
 	vite: {
